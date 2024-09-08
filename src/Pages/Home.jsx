b763036aed6c4fa80620e2290480b1a79379986e
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-09-09 01:43:42 +0800

    Created a Navbar and added changes to the Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,25 @@
 function Home() {
 
-  
-    return (
-      <>
-      <div>
-        <h1>This is the Home page!</h1>
+
+  return (
+    <>
+    <div class="relative max-w-xl mx-auto mt-20">
+        <img class="h-64 w-full object-cover rounded-md" src="https://images.unsplash.com/photo-1680725779155-456faadefa26" alt="Random image" />
+        <div class="absolute inset-0 bg-gray-700 opacity-60 rounded-md"></div>
+        <div class="absolute inset-0 flex items-center justify-center">
+          <h2 class="text-white text-3xl font-bold">Get Lost in Modsaduntains</h2>
+        </div>
       </div>
-        
-      </>
-    )
-  }
-  
-  export default Home
+
+    <div>
+        <h1 className="text-5xl mt-40"><b className="text-yellow-500">Fuel Your Brand's Growth </b>
+          with Cutting-Edge Digital Marketing Solutions</h1>
+    </div>
+
+      
+
+    </>
+  )
+}
+
+export default Home
