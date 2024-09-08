e75c3fb5fadeff294f5a933859b39a7beadcfda2
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-09-08 22:34:14 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h1>This is the main page!</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
