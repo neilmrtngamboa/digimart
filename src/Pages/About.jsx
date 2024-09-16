d69f37a29a840661aaba8404bb5748e274ffb7b4
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-09-16 14:11:42 +0800

    Customized the About Us page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,171 @@
 function About() {
 
-  
-    return (
-      <>
-      <div>
-        <h1>This is the About page!</h1>
+
+  return (
+    <>
+      <section>
+
+        <div className="bg-gray-600 p-5">
+
+          <div className="grid grid-cols-1 lg:grid-cols-2 mt-10 gap-9">
+
+            <div className="basis-1/2 p-3">
+
+              <img className="rounded-lg" src="https://a.storyblok.com/f/96206/3008x2177/db8af7aacf/ucdpa-digital-marketing-career-paths.jpg/m/900x0/filters:quality/(60/)" alt="" />
+
+            </div>
+
+            <div className="basis-1/2 mt-16 p-10">
+
+              <h1 className="text-white text-5xl font-thin">About Us</h1>
+              <p className="mt-3 text-lg text-gray-400">
+                Driven by a passion for seamless user experiences, we've meticulously curated pagedone to empower creators, designers, and developers alike. Our mission is to provide a comprehensive toolkit, enabling you to build intuitive, beautiful interfaces that resonate with users on every interaction.
+              </p>
+
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className="bg-gray-700 p-5">
+
+          <div className="grid grid-cols-1 lg:grid-cols-2 mt-10 gap-9">
+
+            <div className="basis-1/2 p-10 mx-auto">
+
+              <h1 className="text-white text-5xl font-thin">Growing brands since 2023</h1>
+              <p className="mt-3 text-lg text-gray-400">
+                DigiMart isn't always about rules and guidelines; it's about growth and committment to what you can do to help brands grow.
+                Every element, from the tiniest detail to the grandest layout, is meticulously crafted to enhance functionality and elevate user satisfaction.
+              </p>
+
+
+            </div>
+
+            <div className="basis-1/2 p-3">
+
+              <img className="rounded-lg" src="https://foundersguide.com/wp-content/uploads/2018/09/pexels-photo-1249158.jpeg" alt="" />
+
+            </div>
+
+          </div>
+
+        </div>
+
+      <div className="bg-gray-900 p-10">
+
+        <div className="grid grid-cols-1 lg:grid-cols-3  gap-3">
+
+          <div className="bg-gray-800 shadow-inner rounded-3xl p-10">
+
+            <p className="text-yellow-400 font-extrabold text-4xl text-center"> 121+</p>
+            <p className="text-white text-center text-xl font-semibold">Projects Completed</p>
+            <p className="text-gray-400 text-center font-extralight text-md mt-2">Many brand owners or business owners have trusted us
+              with their brand/business growth</p>
+
+          </div>
+
+          <div className="bg-gray-800 shadow-inner rounded-3xl p-10">
+
+          <p className="text-yellow-400 font-extrabold text-4xl text-center"> 100% </p>
+          <p className="text-white text-center text-xl font-semibold">Brand Growth</p>
+          <p className="text-gray-400 text-center font-extralight text-md mt-2">Many brand owners or business owners have trusted us
+              with their brand/business growth</p>
+
+          </div>
+
+          <div className="bg-gray-800 shadow-inner rounded-3xl p-10">
+
+          <p className="text-yellow-400 font-extrabold text-4xl text-center"> 170%</p>
+          <p className="text-white text-center text-xl font-semibold">Company Growth</p>
+          <p className="text-gray-400 text-center font-extralight text-md mt-2">Many brand owners or business owners have trusted us
+              with their brand/business growth</p>
+
+          </div>
+
+        </div>
+
       </div>
+
+      <section className="bg-slate-200">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+          <figure className="max-w-screen-md mx-auto">
+            <blockquote>
+              <p className="text-2xl font-medium text-gray-900">"DigiMart delivered quality results and it helped my business to have more reach locally."</p>
+            </blockquote>
+            <figcaption className="flex items-center justify-center mt-6 space-x-3">
+              <img className="w-6 h-6 rounded-full" src="https://s3p.sofifa.net/ab1ad94fb959fe55a6b391cadec721db570f0172.png"/>
+              <div className="flex items-center divide-x-2 divide-gray-500 ">
+                <div className="pr-3 font-medium text-gray-900">George Gray</div>
+                <div className="pl-3 text-sm font-light text-gray-500">Local Business Owner, Food2Go</div>
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+      </section>
+
+      <section className="bg-gray-300 shadow-inner">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+          <figure className="max-w-screen-md mx-auto">
+            <blockquote>
+              <p className="text-2xl font-medium text-gray-900">"My website gained more traffic since working with DigiMart. Not only that, that website traffic
+              that we gained converted into sales!"</p>
+            </blockquote>
+            <figcaption className="flex items-center justify-center mt-6 space-x-3">
+              <img className="w-6 h-6 rounded-full" src="https://d2zia2w5autnlg.cloudfront.net/176388/profile-6267f5eb5c407-small"/>
+              <div className="flex items-center divide-x-2 divide-gray-500 ">
+                <div className="pr-3 font-medium text-gray-900">Jett Lorenzo</div>
+                <div className="pl-3 text-sm font-light text-gray-500">Owner, MotoTorque</div>
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+      </section>
+
+      <section className="bg-slate-200">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+          <figure className="max-w-screen-md mx-auto">
+            <blockquote>
+              <p className="text-2xl font-medium text-gray-900">"Working with DigiMart helped us achieve unprecedented growth. We asked and they delivered."</p>
+            </blockquote>
+            <figcaption className="flex items-center justify-center mt-6 space-x-3">
+              <img className="w-6 h-6 rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr7HPF_uJQhBRebp_Jqgj8ROEJ-73H0LUMHQ&s"/>
+              <div className="flex items-center divide-x-2 divide-gray-500 ">
+                <div className="pr-3 font-medium text-gray-900">Kylian Mbappe</div>
+                <div className="pl-3 text-sm font-light text-gray-500">Online Business Owner, Madridista</div>
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+      </section>
+
+      <div className="bg-gray-600 pb-10">
         
-      </>
-    )
-  }
-  
-  export default About
+        <div className="flex flex-col items-center p-0">
+
+          <div className="rounded bg-gray-600 drop-shadow-2xl shadow-gray-800 p-5 mt-10 w-11/12 flex flex-col items-center">
+
+          <div className=" text-white font-semibold text-5xl text-center">Let's talk business</div>
+          <p className=" text-gray-400 font-extralight mt-5 text-center">Contact us now and let's talk about your brand's growth potential!</p>
+          <button className="bg-yellow-400 hover:bg-yellow-500 text-slate-900 font-bold py-2 px-4 border-b-4
+           border-yellow-500 hover:border-yellow-600 hover:text-white rounded mt-5">Talk with us</button>
+
+          </div>
+
+          
+
+        </div>
+
+      </div>
+
+      
+
+    </section>
+
+    </>
+  )
+}
+
+export default About
