ca85e02d58216673c277842d148c1ebcf73c4d2a
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-09-17 20:26:07 +0800

    Added AoS on the Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react"
 import { Link } from "react-router-dom"
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+
 
 function Home() {
+
+  useEffect(() => {
+    AOS.init();
+  }, [])
 
 
   return (
@@ -13,16 +22,16 @@ function Home() {
         <p className="text-white mt-2 text-lg">Drive engagement and boost your online presence with our tailored social media marketing strategies at DigiMart.</p>
         <div className="flex flex-row">
           <Link to='about'>
-          <button class="outline outline-white text-white hover:bg-white hover:text-black font-bold py-2 px-4 rounded-full mt-3">
-            Get Started
-          </button>
+            <button class="outline outline-white text-white hover:bg-white hover:text-black font-bold py-2 px-4 rounded-full mt-3">
+              Get Started
+            </button>
           </Link>
           <Link to='contact'>
-          <button class="bg-yellow-500 hover:bg-yellow-600 text-slate-900 hover:text-white font-bold py-2 px-4 rounded-full mt-3 ms-2">
-            Get a Free Consultation!
-          </button>
+            <button class="bg-yellow-500 hover:bg-yellow-600 text-slate-900 hover:text-white font-bold py-2 px-4 rounded-full mt-3 ms-2">
+              Get a Free Consultation!
+            </button>
           </Link>
-          
+
         </div>
 
       </div>
@@ -35,21 +44,24 @@ function Home() {
 
         <div className="sm:flex mt-5 sm:gap-3 justify-center">
 
-          <div className="rounded shadow-xl ms-2 bg-slate-100 p-5 basis-1/4 text-center">
+          <div className="rounded shadow-xl ms-2 bg-slate-100 p-5 basis-1/4 text-center"
+            data-aos="zoom-in" data-aos-duration='2000'>
             <img className="mx-auto rounded shadow-lg" src="https://images.stockcake.com/public/7/f/d/7fd721c1-b4a0-475f-aa38-f3c84a00c172_medium/social-media-overflow-stockcake.jpg" alt="" />
             <p className="font-bold mt-2">Social Media Management</p>
             <p className="font-extralight text-base text-gray-700">Strategic social media campaigns to engage your audience</p>
 
           </div>
 
-          <div className="rounded shadow-xl ms-2 bg-slate-100  p-5 basis-1/4 text-center">
+          <div className="rounded shadow-xl ms-2 bg-slate-100  p-5 basis-1/4 text-center"
+            data-aos="zoom-in" data-aos-duration='2000'>
             <img className="mx-auto rounded shadow-lg" src="https://d28ku8nzmkcjr6.cloudfront.net/cache/db/67/db6702bdc80c7e310edf1d94c0599c86.webp" alt="" />
             <p className="font-bold mt-2">SEO Optimization</p>
             <p className="font-extralight text-base">Help your brand to rank in search engines</p>
 
           </div>
 
-          <div className="rounded shadow-xl ms-2 bg-slate-100  p-5 basis-1/4 text-center">
+          <div className="rounded shadow-xl ms-2 bg-slate-100  p-5 basis-1/4 text-center"
+            data-aos="zoom-in" data-aos-duration='2000'>
             <img className="mx-auto rounded shadow-lg" src="https://era9r2jcsww.exactdn.com/wp-content/uploads/2024/03/Cover-3-4-256x256.jpg?strip=all&lossy=1&quality=80&ssl=1" alt="" />
             <p className="font-bold mt-2">Paid Ads</p>
             <p className="font-extralight text-base">Reach likely customers with your products, features, and services in the best ad formats.</p>
@@ -69,7 +81,7 @@ function Home() {
 
           </div>
 
-          <div className="basis-1/2">
+          <div className="basis-1/2" data-aos="fade-left" data-aos-duration="2000">
             <h3 className="text-white font-extrabold text-5xl mt-36">Empowering Brands, Creating Connections</h3>
             <p className="text-white font-thin mt-5 break-normal text-lg">DigiMart is dedicated to helping brands succeed with innovative social media strategies. We focus on engagement and growth.</p>
             <p className="text-white font-thin mt-5 break-normal text-lg">DigiMart is driven by a passion for enhancing brands through effective digital marketing.</p>
@@ -89,7 +101,8 @@ function Home() {
 
         <div className="sm:flex sm:gap-5 justify-center mt-5">
 
-          <div class="max-w-sm bg-gray-800 rounded-lg basis-1/2 shadow-inner shadow-md">
+          <div class="max-w-sm bg-gray-800 rounded-lg basis-1/2 shadow-inner shadow-md"
+            data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
             <img className="rounded-t-lg mx-auto" src="https://static.dynamitenews.com/images/2022/05/31/health-5-essential-guidelines-while-working-on-computer/6295cffb4cbf0.jpg" alt="" />
             <div class="p-3">
               <h5 class="mb-2 text-2xl font-light tracking-tight text-white ">Boost rankings and drive traffic to your website</h5>
@@ -98,7 +111,8 @@ function Home() {
             </div>
           </div>
 
-          <div class="max-w-sm bg-gray-800 rounded-lg shadow basis-1/2">
+          <div class="max-w-sm bg-gray-800 rounded-lg shadow basis-1/2"
+            data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
             <img className="rounded-t-lg mx-auto" src="https://dluhcdigital.blog.gov.uk/wp-content/uploads/sites/222/2024/03/FSD-Away-Day-London_V2-9595-620x386.jpg" alt="" />
             <div class="p-3">
               <h5 class="mb-2 text-2xl font-light tracking-tight text-white">
@@ -115,11 +129,11 @@ function Home() {
       <section className="bg-gray-800 p-5">
         <p className="text-white text-4xl text-center font-extrabold">How we can help you?</p>
         <p className="text-gray-400 text-lg mt-3 text-center mb-5">Here are a few of the questions we get the most. If you don't see what's on your mind, reach out to us anytime on phone, chat, or email.</p>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4" data-aos="fade-up" data-aos-duration='3000'>
 
-          <div className="p-5 text-white">
+          <div d className="p-5 text-white">
             <p className="flex items-center font-bold mb-3 text-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
               </svg>
               Do I need digital marketing for my business?</p>
@@ -149,7 +163,7 @@ function Home() {
 
           <div className="p-5 text-white">
             <p className="flex items-center font-bold mb-3 text-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
               </svg>
               How will you develop a strategy for my business?</p>
@@ -159,7 +173,7 @@ function Home() {
 
           <div className="p-5 text-white">
             <p className="flex items-center font-bold mb-3 text-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
               </svg>
               Do you work with small businesses?</p>
@@ -169,7 +183,7 @@ function Home() {
 
           <div className="p-5 text-white">
             <p className="flex items-center font-bold mb-3 text-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 me-2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
               </svg>
               Can you guarantee first-page rankings on Google?</p>
@@ -183,15 +197,15 @@ function Home() {
 
       <section className="bg-gray-950 pb-14">
 
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center" data-aos='flip-up' data-aos-duration='3000'>
 
           <div className=" text-white font-thin text-5xl mt-16 text-center">Boost your brand productivity.</div>
           <p className=" text-gray-400 font-extralight mt-5 text-center">Contact us now and let's talk about your brand's growth potential!</p>
           <Link to='contact'>
-          <button className="bg-yellow-400 hover:bg-yellow-500 text-slate-900 font-bold py-2 px-4 border-b-4
+            <button className="bg-yellow-400 hover:bg-yellow-500 text-slate-900 font-bold py-2 px-4 border-b-4
            border-yellow-500 hover:border-yellow-600 hover:text-white rounded mt-5">Grow with us!</button>
           </Link>
-          
+
 
         </div>
 
