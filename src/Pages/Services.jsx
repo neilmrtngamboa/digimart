92abb80a95aca87d9795ec1a388dd60dc71e95ef
neilmrtngamboa <nmg.gamboa@gmail.com>
2024-09-18 09:53:13 +0800

    Customized the Services Page

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,11 +1,143 @@
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 
-function Services () {
+function Services() {
+
+    useEffect(() => {
+        AOS.init();
+    }, [])
 
 
     return (
-        <section>
-        
+        <section className="bg-gray-700 p-5">
+
+            <div className="text-center mt-7 p-5" data-aos="fade-up" data-aos-duration='1000'>
+                <h1 className="text-white font-semibold text-7xl">Our Services</h1>
+                <p className="text-gray-400 font-light text-xl my-7">Elevate your brand’s online presence with our expert social media marketing services at DigiMart.
+                    We specialize in creating tailored strategies that engage your audience and boost your visibility!</p>
+
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 pb-10" data-aos="fade-down" data-aos-duration='1000'>
+
+                <div className="bg-gray-700 p-5 basis-1/2 rounded-xl border border-gray-600 shadow-2xl">
+                    <h2 className="text-white font-bold text-2xl">Social Media Marketing</h2>
+                    <p className="text-gray-400 text-md my-3">Our social media marketing service includes in-depth audience analysis, content creation,
+                        and real-time engagement to maximize your brand’s reach and impact on various platforms.</p>
+
+                    <ul className="font-semibold text-gray-200 mt-16">
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Content Creation</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Social Media Account Management</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Paid Social Media Advertising</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Social Media Analytics and Reporting</li>
+                    </ul>
+                    <Link to='/contact'>
+                        <button class="bg-transparent hover:bg-white text-white font-semibold hover:text-gray-800 py-2 px-4 border border-white hover:border-transparent 
+                        rounded-full my-20 transition duration-500">
+                            Get Started
+                        </button>
+                    </Link>
+
+                </div>
+
+                <div className="bg-gray-700 p-5 basis-1/2 rounded-xl border border-gray-600 shadow-2xl">
+                    <h2 className="text-white font-bold text-2xl">SEO Optimization</h2>
+                    <p className="text-gray-400 text-md my-3">These SEO services cover the full spectrum of optimization techniques to help businesses improve their search engine rankings and drive organic traffic.</p>
+
+                    <ul className="font-semibold text-gray-200 mt-16">
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            SEO Audit</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Keyword Research and Strategy</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            On-Page SEO Optimization</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Link Building</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Local SEO</li>
+                    </ul>
+                    <Link to='/contact'>
+                        <button class="bg-transparent hover:bg-white text-white font-semibold hover:text-gray-800 py-2 px-4 border border-white hover:border-transparent 
+                        rounded-full my-10 transition duration-500">
+                            Get Started
+                        </button>
+                    </Link>
+
+                </div>
+
+                <div className="bg-gray-700 p-5 basis-1/2 rounded-xl border border-gray-600 shadow-2xl">
+                    <h2 className="text-white font-bold text-2xl">Paid Ads</h2>
+                    <p className="text-gray-400 text-md my-3">These paid ad services provide a comprehensive approach to driving traffic, leads, and sales for businesses across various platforms.</p>
+
+                    <ul className="font-semibold text-gray-200 mt-20">
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Google Ads Management</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Social Media Advertising</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            PPC Campaign Strategy</li>
+                        <li className="flex items-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-yellow-500 me-1">
+                                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                            </svg>
+                            Video Ads Management</li>
+                    </ul>
+                    <Link to='/contact'>
+                        <button class="bg-transparent hover:bg-white text-white font-semibold hover:text-gray-800 py-2 px-4 border border-white hover:border-transparent 
+                        rounded-full my-20 transition duration-500">
+                            Get Started
+                        </button>
+                    </Link>
+
+                </div>
+
+            </div>
+
+
+
         </section>
     )
 
